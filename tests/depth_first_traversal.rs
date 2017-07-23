a38e692e408c72743c4605ad0dd5f3a15a5fafde
Harry Braviner <harry.braviner@gmail.com>
2017-07-23 22:55:36 +0100

    Added a second traversal test
    Added a test for traversal of a more complicated graph.

--- tests/depth_first_traversal.rs
+++ tests/depth_first_traversal.rs
@@ -33,3 +33,27 @@ fn traversal_order() {
     assert_eq!(vec![2, 0, 1], discovery_order);
     assert_eq!(vec![1, 0, 2], processed_order);
 }
+
+#[test]
+fn traversal_order_2() {
+    let g = graph_builders::from_file("test_data/graph3").unwrap();
+
+    let mut discovery_order = Vec::<usize>::new();
+    let mut processed_order = Vec::<usize>::new();
+
+    g.depth_first_iter_from_index(|i| discovery_order.push(*i),
+                                  |i| processed_order.push(*i),
+                                  |_, _| { },
+                                  0);
+    assert_eq!(vec![0, 1, 2, 3, 4, 5], discovery_order);
+    assert_eq!(vec![4, 3, 2, 1, 5, 0], processed_order);
+
+    discovery_order.clear();
+    processed_order.clear();
+    g.depth_first_iter_from_index(|i| discovery_order.push(*i),
+                                  |i| processed_order.push(*i),
+                                  |_, _| { },
+                                  2);
+    assert_eq!(vec![2, 1, 0, 3, 4, 5], discovery_order);
+    assert_eq!(vec![4, 3, 5, 0, 1, 2], processed_order);
+}
