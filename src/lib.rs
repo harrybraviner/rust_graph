ba98c0b7c9cc2fced2cc53f9442c6a06a0230e5b
Harry J. Braviner <harry.braviner@gmail.com>
2017-07-23 21:38:26 +0100

    DFS compiles - incomplete and no tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,20 @@ pub struct Graph<T> where T : Clone + Eq + Hash {
     adjacency_list : Vec<Vec<usize>>,
 }
 
+#[derive(PartialEq, Eq, Clone)]
+pub enum BFSTraversalState {
+    Undiscovered,
+    Discovered,
+    Processed,
+}
+
+#[derive(PartialEq, Eq, Clone)]
+pub enum DFSTraversalState {
+    Undiscovered,
+    Processing (usize),
+    Processed  (usize, usize),
+}
+
 impl<T> Graph<T> where T : Clone + Eq + Hash {
 
     pub fn new() -> Graph<T> {
@@ -63,15 +77,9 @@ impl<T> Graph<T> where T : Clone + Eq + Hash {
                                                 mut process_edge : G,
                                                 root_index : usize)
         where F : FnMut(&T) -> (), G : FnMut(&T, &T) -> () {
-        #[derive(PartialEq, Eq, Clone)]
-        enum NodeState {
-            Undiscovered,
-            Discovered,
-            Processed,
-        };
 
-        let mut node_states = vec![NodeState::Undiscovered; self.number_of_vertices()];
-        node_states[root_index] = NodeState::Discovered;
+        let mut node_states = vec![BFSTraversalState::Undiscovered; self.number_of_vertices()];
+        node_states[root_index] = BFSTraversalState::Discovered;
 
         let mut nodes_to_process = VecDeque::<usize>::new();
         nodes_to_process.push_back(root_index);
@@ -80,14 +88,14 @@ impl<T> Graph<T> where T : Clone + Eq + Hash {
             for dest_node in &self.adjacency_list[current_node] {
                 // Note - this does both edges in both directions for an undirected graph.
                 process_edge(&self.nodes[current_node], &self.nodes[*dest_node]);
-                if node_states[*dest_node] == NodeState::Undiscovered {
-                    node_states[*dest_node] = NodeState::Discovered;
+                if node_states[*dest_node] == BFSTraversalState::Undiscovered {
+                    node_states[*dest_node] = BFSTraversalState::Discovered;
                     nodes_to_process.push_back(*dest_node);
                 }
             }
 
             process_vertex(&self.nodes[current_node]);
-            node_states[current_node] = NodeState::Processed;
+            node_states[current_node] = BFSTraversalState::Processed;
         }
     }
 
@@ -100,6 +108,61 @@ impl<T> Graph<T> where T : Clone + Eq + Hash {
         where F : FnMut(&T) -> (), G : FnMut(&T, &T) -> () {
         let root_index = self.index_from_node(root_node);
         self.breadth_first_iter_from_index(process_vertex, process_edge, root_index)
+    }
+
+    pub fn depth_first_iter_from_index<F, G> (&self,
+                                              process_vertex_early : F,
+                                              process_vertex_late  : F,
+                                              process_edge         : G,
+                                              root_node : usize)
+        where F : FnMut(&T) -> (), G : FnMut(&T, &T) -> () {
+
+        let mut discovery_state = vec![DFSTraversalState::Undiscovered; self.number_of_vertices()];
+        let mut parent : Vec<Option<usize>> = vec![None; self.number_of_vertices()];
+        let mut time = 0usize;  // FIXME - if we have more than MAX_USIZE/2 nodes in the graph, this will overflow!
+        let mut processing_stack = VecDeque::<usize>::new();
+
+        // Setup out stack so that we start with the specified root node
+        //processing_stack.push_back(root_node);
+
+        // Keeping an explict stack turns out to be quite tricky.
+        // A recursive function (that changes all of the above mutable state)
+        // seems like a much nicer way of doing this.
+        // FIXME - calls to processing functions?
+        // FIXME - make the recursion below work!
+        fn inner_dfs(current_node    : usize,
+                     adjacency_list  : &    Vec<Vec<usize>>,
+                     discovery_state : &mut Vec<DFSTraversalState>,
+                     parent          : &mut Vec<Option<usize>>,
+                     time            : &mut usize) {
+            let entry_time : usize = *time;
+            discovery_state[current_node] = DFSTraversalState::Processing(entry_time);
+            *time += 1;
+            for dest_node in &adjacency_list[current_node] {
+                if discovery_state[*dest_node] == DFSTraversalState::Undiscovered {
+                    parent[*dest_node] = Some(current_node);
+                    inner_dfs(*dest_node, adjacency_list, discovery_state, parent, time);
+                    // FIXME - process_edge??
+                }
+            }
+            discovery_state[current_node] = DFSTraversalState::Processed(entry_time, *time);
+            *time += 1;
+        };
+
+        inner_dfs(root_node, &self.adjacency_list, &mut discovery_state, &mut parent, &mut time);
+
+//        while let Some(current_node) = processing_stack.pop_back() {
+//            discovery_state[current_node] = DFSTraversalState::Processing(time);
+//            time += 1;
+//            for dest_node in &self.adjacency_list[current_node] {
+//                if discovery_state[*dest_node] == DFSTraversalState::Undiscovered {
+//                    parent[*dest_node] = Some(current_node);    // FIXME - this doesn't work because of processing order!
+//                    processing_stack.push_back(*dest_node);     // FIXME - and this will put some nodes of the stack multiple times!
+//                }
+//            }
+//        }
+
+        panic!("Not implemented")
     }
 }
 
